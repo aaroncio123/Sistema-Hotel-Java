a5db1c6cdc485d2cebfc99e3c55ae2bbd4ea9b86
Aaron <aaronhorna03@gmail.com>
2026-06-25 00:45:14 -0500

    Eliminando declaracion explicitade api key de chatbiot

---
+++ src/main/java/gestion/com/hotel/controllers/PreguntaRequest.java
@@ -0,0 +1,13 @@
+package gestion.com.hotel.controllers;
+
+public class PreguntaRequest {
+    private String mensaje;
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
---
+++ src/main/java/gestion/com/hotel/services/GeminiService.java
@@ -0,0 +1,66 @@
+package gestion.com.hotel.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class GeminiService {
+    @Value("${gemini.api.key}")
+    private String apiKey;
+
+    @Value("${gemini.api.url}")
+    private String apiUrl;
+
+    public String preguntar(String mensaje) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            String url = apiUrl + "?key=" + apiKey;
+
+            String body = """
+            {
+              "contents": [
+                {
+                  "parts": [
+                    {
+                      "text": "%s"
+                    }
+                  ]
+                }
+              ]
+            }
+            """.formatted(mensaje.replace("\"", "\\\""));
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<String> request = new HttpEntity<>(body, headers);
+
+            ResponseEntity<String> response =
+                    restTemplate.postForEntity(url, request, String.class);
+
+         ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response.getBody());
+
+        return root
+                .path("candidates")
+                .get(0)
+                .path("content")
+                .path("parts")
+                .get(0)
+                .path("text")
+                .asText();
+
+        } catch (Exception e) {
+            return "No pude obtener una respuesta de Gemini.";
+        }
+    }
+}
